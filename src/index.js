665250d7521ae84d842d17bf8c0507d80b6d79a2
Srinivas Akella <srinivas.prasad@teknotrait.com>
2022-11-29 00:31:40 +0530

    ua uwa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material";
 import theme from "./Components/theme";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const app = express();
+app.use(express.static("reactFolderName/build"));
+app.get("*", (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, "reactFolderName", "build", "index.html")
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
