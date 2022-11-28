1e546ef8adebb9827a956c19d421a12739d94089
Srinivas Akella <srinivas.prasad@teknotrait.com>
2022-11-29 00:41:28 +0530

    lat

--- src/index.js
+++ src/index.js
@@ -9,14 +9,6 @@ import theme from "./Components/theme";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const app = express();
-app.use(express.static("reactFolderName/build"));
-app.get("*", (req, res) =>
-  res.sendFile(
-    path.resolve(__dirname, "reactFolderName", "build", "index.html")
-  )
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
